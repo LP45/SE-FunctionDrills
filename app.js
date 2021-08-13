b19e85c58b14d528aa788b1ce250481946ad3fdf
Dan Lo <nflwwe71@gmail.com>
2021-08-13 15:39:23 -0400

    1st draft

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+sumOfNums = (nums) =>{
+    if(nums < 0){
+        return 0
+    }
+}
+
+//LOOP
+//Input an array of numbers
+//if array is 0 or less than 0 return 0
+// if not zero add numbers that were passed in 
+
+//input a word and a number
+
+
+
+
+//if statemenet`
